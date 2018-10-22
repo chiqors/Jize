2aa3e4dca640a8c00a07df32a14f900c759a1a0e
HAN-105 <bandi_nada@live.com>
2018-10-22 12:59:51 +0700

    - Added Controllers System

--- index.php
+++ index.php
@@ -7,18 +7,15 @@ $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 // Route it up!
 switch ($request_uri[0]) {
     // Home page
-    case load_url().'':
-		include __DIR__ . '/views/templates/v_header.php';
-        include __DIR__ . '/views/v_index.php';
-		include __DIR__ . '/views/templates/v_footer.php';
-        break;
-	case load_url().'/':
+    case load_url().'' || load_url().'/index' || load_url().'/':
+        include __DIR__ . '/controllers/c_index.php';
 		include __DIR__ . '/views/templates/v_header.php';
         include __DIR__ . '/views/v_index.php';
 		include __DIR__ . '/views/templates/v_footer.php';
         break;
     // About page
     case load_url().'/about':
+        include __DIR__ . '/controllers/c_about.php';
 		include __DIR__ . '/views/templates/v_header.php';
         include __DIR__ . '/views/v_about.php';
 		include __DIR__ . '/views/templates/v_footer.php';
