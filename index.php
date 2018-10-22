7abb16489c61c3e0547aa6dac24659f59f504609
HAN-105 <bandi_nada@live.com>
2018-10-22 17:06:42 +0700

    - Test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include 'configs/database.php';
 include 'configs/libraries.php';
 // Grabs the URI and breaks it apart in case we have querystring stuff
-$request_uri = explode('/?', $_SERVER['REQUEST_URI'], 2);
+$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
 // Route it up!
 switch ($request_uri[0]) {
