1e79c744adac3d8b511564a4dc33d57ec9224655
HAN-105 <bandi_nada@live.com>
2018-10-22 15:13:10 +0700

    - Added sample_user.png - Added title system for web title name - Added login page view

--- views/templates/v_header.php
+++ views/templates/v_header.php
@@ -4,7 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jize - The SCS Website</title>
+    <title>Jize - 
+        <?php
+            $uri = explode('/?', $_SERVER['REQUEST_URI'], 2);
+            switch ($request_uri[0]) {
+                // Home page
+                case load_url().'':
+                case load_url().'/index':
+                case load_url().'/':
+                    echo "Home";
+                    break;
+                // About page
+                case load_url().'/about':
+                    echo "About";
+                    break;
+                // Login page
+                case load_url().'/login':
+                    echo "Login";
+                    break;
+                // Everything else
+                default:
+                    echo "Error 404";
+                    break;
+            }
+        ?>
+    </title>
     <link href="<?php echo base_url() ?>/assets/css/style.css" rel="stylesheet" />
 </head>
 <body>
