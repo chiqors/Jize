8370b7671153cd3ce98fa66ce5e1a057865b4336
HAN-105 <bandi_nada@live.com>
2018-10-22 17:00:39 +0700

    - Tried

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'configs/database.php';
+include 'configs/libraries.php';
 // Grabs the URI and breaks it apart in case we have querystring stuff
 $request_uri = explode('/?', $_SERVER['REQUEST_URI'], 2);
 
