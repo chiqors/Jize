9d989f52fe52c0401766fe6d89eb52f89cab5d7e
tendy92 <hermantendy92@gmail.com>
2018-10-26 16:16:00 +0700

    checkout

---
+++ views/guest/v_checkout.php
@@ -0,0 +1,82 @@
+<div class="content-wrapper mb-5">
+	<div class="container">
+		<div class="row">
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<h1>Checkout</h1>
+			<table class="table table-striped bg-white table-cart">
+				<tr>
+					<td>First Name</td>
+					<td>Last Name</td>
+				</tr>
+				<tr>
+					<td colspan="2">Username</td>
+				</tr>
+				<tr>
+					<td colspan="2">Example.example.com</td>
+				</tr>
+				<tr>
+					<td colspan="2">Address</td>
+				</tr>
+				<tr>
+					<td colspan="2">Address 2 (Optional)</td>
+				</tr>
+				<tr>
+					<td>
+						<select>
+							<option>Choose..</option>
+							<option value="Indonesia">Indonesia</option>
+						</select>
+					</td>
+					<td>
+						<select>
+							<option>Choose..</option>
+							<option value="Indonesia">Bandung</option>
+						</select>
+					</td>
+				</tr>
+				
+			</table>
+			</div>
+			<div class="col-md-4">
+				<h2>Your Cart</h2>
+				<div>
+					<ul class="list-group mb-3 z-depth-1">
+						<li class="list-group-item d-flex justify-content-between lh-condensed">
+							<div>
+								<h6 class="my-0">Product Name</h6>
+								<small class="text-muted">Brief Description</small>
+							</div>						
+							<span class="text-muted">Rp. 300rb</span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between lh-condensed">
+							<div>
+								<h6 class="my-0">Second Name</h6>
+								<small class="text-muted">Brief Description</small>
+							</div>							
+							<span class="text-muted">Rp. 300rb</span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between lh-condensed">
+							<div>
+								<h6 class="my-0">Thrid Item</h6>
+								<small class="text-muted">Brief Description</small>
+							</div>							
+							<span class="text-muted">Rp. 300rb</span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between">
+							<h6 class="my-0">Total</h6>
+							<strong>Rp. 900r</strong>
+						</li>
+					</ul>
+					<ul  class="list-group mb-3 z-depth-1">
+						<div class="input-group">
+							<input type="button" class="btn-block" value="Continue To Checkout"style="background-color: #3958a8;color: #fff;padding: 11px 35px;">
+						</div>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+				
