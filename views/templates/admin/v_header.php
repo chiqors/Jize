bc3576e4c22b69cad7af9d7e20bf4fe3696c6662
HAN-105 <bandi_nada@live.com>
2018-10-28 18:39:24 +0700

    - Added admin dashboard page with its example layout

---
+++ views/templates/admin/v_header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Jize - <?= $title ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?= site_url('/assets/css/bootstrap.min.css') ?>">
+
+    <!-- Custom styles for this template -->
+    <link href="<?= site_url('/assets/css/dashboard.css') ?>" rel="stylesheet">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#">Sign out</a>
+        </li>
+      </ul>
+    </nav>
