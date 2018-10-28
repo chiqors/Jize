2e38ca9896d82e2a49e4bb8b4a066ffa39a2f5ea
wisnu41 <wisnukm41@gmail.com>
2018-10-28 18:52:31 +0700

    forgot psw

--- controllers/functions/c_do_forget_password.php
+++ controllers/functions/c_do_forget_password.php
@@ -6,11 +6,14 @@
     $kpassword=$_POST['kpassword'];
     $username=$_POST['user'];
     $email=$_POST['email'];
+    $id=$_SESSION['id_user'];
 
     $users="SELECT * FROM users WHERE email = '$email' AND username = '$username'";
     $query=mysqli_query($mysql,$users);
-    $userspsw="UPDATE users SET password = '$password'";
+    $userspsw="UPDATE users SET password = '$password' WHERE id = '$id'";
     if (mysqli_num_rows($query)>0) {
+        $row = mysqli_fetch_object($query);
+        $_SESSION['id_user'] = $row->id;
         $_SESSION['change_password'] = true;
         header('Location:'. site_url("forget_password"));
     }else {
